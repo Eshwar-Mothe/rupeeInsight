a6b4843b23d848eebb4531019a15dd21f38675f7
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-03-03 13:26:32 +0530

    added images at feature section

--- src/components/LandingPage/SlideSection.jsx
+++ src/components/LandingPage/SlideSection.jsx
@@ -20,46 +20,46 @@ const SlideSection = () => {
                     autoplaySpeed={5000}
                     >
                     <div >
-                        <div className="card-container px-5 py-3 ">
+                        <div className="card-container px-3 py-1 ">
                             <div className='content'>
                                 <p >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis ipsam nam earum qui maxime aliquam, quisquam corporis consequatur quaerat error. Modi quisquam voluptatem magnam omnis veniam numquam dicta deserunt itaque.
                                     Nostrum rerum, voluptatum delectus, atque a culpa, ut facilis iure laudantium velit repellat neque quisquam id. Quidem, possimus rerum aliquid libero, fuga in adipisci necessitatibus ducimus soluta temporibus harum. Earum.</p>
                             </div>
                             <div>
-                                <img className='image' src="" alt="" width={300} height={300} />
+                                <img className='image' src="./feature1.png" alt="" style={{width:'100%',objectFit:'cover'}} />
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="card-container px-5 py-3 ">
+                        <div className="card-container px-3 py-1 ">
                             <div className='content'>
                                 <p  >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis ipsam nam earum qui maxime aliquam, quisquam corporis consequatur quaerat error. Modi quisquam voluptatem magnam omnis veniam numquam dicta deserunt itaque.
                                     Nostrum rerum, voluptatum delectus, atque a culpa, ut facilis iure laudantium velit repellat neque quisquam id. Quidem, possimus rerum aliquid libero, fuga in adipisci necessitatibus ducimus soluta temporibus harum. Earum.</p>
                             </div>
                             <div>
-                                <img className='image' src="" alt="" width={300} height={300} />
+                                <img className='image' src="./feature2.png" alt="" style={{width:'100%',objectFit:'cover'}}/>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="card-container px-5 py-3 ">
+                        <div className="card-container px-3 py-1 ">
                             <div className='content'>
                                 <p  >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis ipsam nam earum qui maxime aliquam, quisquam corporis consequatur quaerat error. Modi quisquam voluptatem magnam omnis veniam numquam dicta deserunt itaque.
                                     Nostrum rerum, voluptatum delectus, atque a culpa, ut facilis iure laudantium velit repellat neque quisquam id. Quidem, possimus rerum aliquid libero, fuga in adipisci necessitatibus ducimus soluta temporibus harum. Earum.</p>
                             </div>
                             <div>
-                                <img className='image' src="" alt="" width={300} height={300} />
+                                <img className='image' src="./feature3.png" alt="" style={{width:'100%',objectFit:'cover'}} />
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="card-container px-5 py-3 ">
+                        <div className="card-container px-3 py-1 ">
                             <div className='content'>
                                 <p  >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis ipsam nam earum qui maxime aliquam, quisquam corporis consequatur quaerat error. Modi quisquam voluptatem magnam omnis veniam numquam dicta deserunt itaque.
                                     Nostrum rerum, voluptatum delectus, atque a culpa, ut facilis iure laudantium velit repellat neque quisquam id. Quidem, possimus rerum aliquid libero, fuga in adipisci necessitatibus ducimus soluta temporibus harum. Earum.</p>
                             </div>
                             <div>
-                                <img className='image' src="" alt="" width={300} height={300} />
+                                <img className='image' src="./feature4.png" alt=""  style={{width:'100%',objectFit:'cover'}} />
                             </div>
                         </div>
                     </div>
